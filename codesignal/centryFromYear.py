dd556bac534a67b21bfd16a5452fa3db0fdaa1d2
junho85 <junho85@gmail.com>
2019-12-30 22:26:13 +0900

    codesignal - centry from year

---
+++ codesignal/centryFromYear.py
@@ -0,0 +1,9 @@
+# https://app.codesignal.com/arcade/intro/level-1/egbueTZRRL5Mm4TXN
+
+import math
+
+def centuryFromYear(year):
+    return math.ceil(year / 100)
+
+print(centuryFromYear(1905))
+print(centuryFromYear(1700))
