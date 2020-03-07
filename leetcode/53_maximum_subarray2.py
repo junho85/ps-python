4e99d9532a13319e6150f5d91c845aa9ee4bd5b0
junho85 <junho85@gmail.com>
2020-03-07 22:24:14 +0900

    leetcode - 53. Maximum Subarray Simple

--- leetcode/53_maximum_subarray2.py
+++ leetcode/53_maximum_subarray2.py
@@ -1,33 +1,11 @@
 class Solution:
     def maxSubArray(self, nums):
-        zero_and_positive_numbers = list(filter(lambda x: (x >= 0), nums))
+        tmp = nums[0]
+        result = nums[0]
 
-        if len(zero_and_positive_numbers) == 0:
-            return max(nums)
-
-        if len(zero_and_positive_numbers) == len(nums):
-            return sum(nums)
-
-        # trim left
-        for (i, num) in enumerate(nums):
-            if num > 0:
-                nums = nums[i:]
-                break
-
-        # trim right
-        for (i, num) in enumerate(nums[::-1]):
-            if num > 0:
-                nums = nums[:len(nums) - i]
-                break
-
-        if len(nums) == 1:
-            return nums[0]
-
-        result = 0
-        tmp = 0
-        for num in nums:
-            tmp = max(0, tmp + num)
-            result = max(result, tmp)
+        for num in nums[1:]:
+            tmp = max(tmp + num, num)
+            result = max(tmp, result)
 
         return result
 
@@ -37,6 +15,7 @@ assert (s.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6)
 assert (s.maxSubArray([-2, 1]) == 1)
 assert (s.maxSubArray([8, -19, 5, -4, 20]) == 21)
 assert (s.maxSubArray([3, -3, 2, -3]) == 3)
+assert (s.maxSubArray([-1]) == -1)
 assert (s.maxSubArray(
     [-57, 9, -72, -72, -62, 45, -97, 24, -39, 35, -82, -4, -63, 1, -93, 42, 44, 1, -75, -25, -87, -16, 9, -59, 20, 5,
      -95, -41, 4, -30, 47, 46, 78, 52, 74, 93, -3, 53, 17, 34, -34, 34, -69, -21, -87, -86, -79, 56, -9, -55, -69, 3, 5,
