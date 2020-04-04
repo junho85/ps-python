05c77318574264b7c9dfed6f7bc6fbbebc968101
junho85 <junho85@gmail.com>
2020-04-04 11:20:31 +0900

    google code jam 2020 QR 1.Vestigium

---
+++ google-code-jam/2020/1_vestigium.py
@@ -0,0 +1,24 @@
+tc = int(input())
+
+def solve(nc, m):
+    k = sum(m[i][i] for i in range(nc))
+    r = 0
+    c = 0
+    for i in range(nc):
+        if len(set(m[i])) != nc:
+            r += 1
+        column = [row[i] for row in m]
+        if len(set(column)) != nc:
+            c += 1
+
+    return str(k) + " " + str(r) + " " + str(c)
+
+
+for t in range(1, tc+1):
+    nc = int(input())
+    m = []
+    for n in range(1, nc+1):
+        row = [int(i) for i in input().split()]
+        m.append(row)
+    ans = solve(nc, m)
+    print("Case #" + str(t) + ": " + ans)
---
+++ google-code-jam/2020/README.md
@@ -0,0 +1,5 @@
+## 1
+```
+cat 1.txt| python 1_vestigium.py
+cat 2.txt| python 2_nesting_depth.py
+```
