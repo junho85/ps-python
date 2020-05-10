5968712f42554436f893ffe43865bacd8e42e092
junho85 <junho85@gmail.com>
2020-05-11 01:01:33 +0900

    gcd, lcm

--- 01.algorithm/gcd_lcm.py
+++ 01.algorithm/gcd_lcm.py
@@ -1,16 +1,19 @@
 from math import gcd
 
-a = [2, 6]
-b = [16, 32, 96]
-# b = [24, 36]
-
 # 최대공약수 직접 구현
-# 유클리드호제법
+
+# 유클리드호제법 recursive
 def gcd_my(a, b):
     if b == 0:
         return a
     else:
-        return gcd_my(b, a%b)
+        return gcd_my(b, a % b)
+
+# 유클리드호제법 while loop
+def gcd_my2(a, b):
+    while b != 0:
+        (a, b) = (b, a % b)
+    return a
 
 # 배열의 최대공약수
 def gcd_array(arr):
@@ -36,10 +39,25 @@ def lcm_array(arr):
             lcm_arr = lcm(lcm_arr, arr[i])
     return lcm_arr
 
-print(gcd_my(16, 32)) # 16
-print(gcd_my(16, 96)) # 16
+print("== gcd ==")
+assert(gcd(4, 6) == 2)
+assert(gcd(6, 4) == 2)
+assert(gcd(16, 32) == 16)
+assert(gcd(16, 96) == 16)
 
-print(gcd_array(b))
-print(lcm(2, 6))
-print(lcm_array(a))
-print(lcm_array(b))
+print("== gcd_my ==")
+assert(gcd_my(4, 6) == 2)
+assert(gcd_my(6, 4) == 2)
+assert(gcd_my(16, 32) == 16)
+assert(gcd_my(16, 96) == 16)
+
+print("== gcd_my2 ==")
+assert(gcd_my2(4, 6) == 2)
+assert(gcd_my2(6, 4) == 2)
+assert(gcd_my2(16, 32) == 16)
+assert(gcd_my2(16, 96) == 16)
+
+assert(gcd_array([16, 32, 96]) == 16)
+assert(lcm(2, 6) == 6)
+assert(lcm_array([2, 6]) == 6)
+assert(lcm_array([16, 32, 96]) == 96)
